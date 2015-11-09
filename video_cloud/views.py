de1d00c3c1a907f75c2076267b9a1352e0922275
Nikolya Leontiev <bently_93@mail.ru>
2015-11-09 19:58:32 +0300

    initial repository

---
+++ video_cloud/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404   
+from django.http import HttpResponse
+from django.template import Context, loader
+
+import random
+
+def index(request):
+    return render_to_response('index.html')
